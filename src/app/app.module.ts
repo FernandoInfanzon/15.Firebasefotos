f2c9cfacb3f07e4a1d45419e3f3f37f0ebf8bd39
Fernando Infanzon <luisfernandoim@gmail.com>
2017-12-15 17:34:12 -0800

    creacion navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { CargaComponent } from './components/carga/carga.component';
 import { FotosComponent } from './components/fotos/fotos.component';
 import { app_routing } from './app.routes';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     CargaComponent,
-    FotosComponent
+    FotosComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
