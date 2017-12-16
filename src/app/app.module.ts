90022f031bffab725fc7b1d6d23fa815dcd9799d
Fernando Infanzon <luisfernandoim@gmail.com>
2017-12-15 17:25:44 -0800

    creacion de archivo de rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
+
 import { AppComponent } from './app.component';
 import { CargaComponent } from './components/carga/carga.component';
 import { FotosComponent } from './components/fotos/fotos.component';
+import { app_routing } from './app.routes';
 
 
 @NgModule({
@@ -14,7 +16,8 @@ import { FotosComponent } from './components/fotos/fotos.component';
     FotosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    app_routing
   ],
   providers: [],
   bootstrap: [AppComponent]
