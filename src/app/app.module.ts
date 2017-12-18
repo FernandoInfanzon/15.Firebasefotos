99d6f4369f188a9cff328eeaf2a87ddfadb245dd
Fernando Infanzon <luisfernandoim@gmail.com>
2017-12-17 19:50:26 -0800

    Creacion del archivo de directivas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { APP_ROUTING} from './app.routes';
 import { NavbarComponent } from './components/navbar/navbar.component';
 
 import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { NgDropFilesDirective } from './directives/ng-drop-files.directive';
 // import { NgDropFilesDirective } from './directives/ng-drop-files.directive';
 
 
@@ -23,6 +24,7 @@ import { AngularFireDatabaseModule } from 'angularfire2/database';
     CargaComponent,
     FotosComponent,
     NavbarComponent,
+    NgDropFilesDirective,
     // NgDropFilesDirective
   ],
   imports: [
